26aae994796bf6d1486f829050b1f85a8d3086da
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-11-30 16:38:45 -0300

    sda

--- painel.php
+++ painel.php
@@ -4,5 +4,15 @@ session_start();
 ?>
 
 <div>
-    <h1>Olá, <?php echo $_SESSION["usuario"];?></h1>
+    <?php
+        if(isset($_SESSION["usuario"])):
+    ?>
+        <h1>Olá, <?php echo $_SESSION["usuario"];?></h1>
+
+        <a href="scriptlogout.php">Logout</a>
+    <?php
+        else:
+            header("Location:login.php");
+        endif;
+    ?>
 </div>
