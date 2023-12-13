3ccbeb554ab807f374103ddf4f57653624109357
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-12-13 15:01:50 -0300

    udpate

--- editar.php
+++ editar.php
@@ -1,150 +1,207 @@
 <?php
-include('conexao.php');
-$con = dbinit();
-session_start();
-ob_start();
-$id = filter_input(INPUT_GET , "id" , FILTER_SANITIZE_NUMBER_INT);
+    session_start();
+    ob_start(); //Limpando o Buffer de saida no redirecionamento
 
-if(empty($id)){
-    $_SESSION['msg'] = '<p>Error : usuario nao encontrado</p>';
-    header("Location:listar.php");
-}
-$query_id = "SELECT id , matricula , nome , email , estatus , dtcadastro FROM usuarios  WHERE id=$id LIMIT 1";
+    //1- Conexão com o Banco de Dados
+    include("conexao.php");
+    $conn = dbinit();
 
-$result_user = $con->prepare($query_id);
+    //2 - Receber o id do usuário através da URL, utilizando o método GET
+    $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
-$result_user->execute();
+    //3 - Verificar se a variável ID não está vazia. (empty)
+    if(empty($id)){
+        $_SESSION['msg'] = "<p style='color:#f00;'>Erro: Usuário não encontrado</p>";
+        header("Location: listar.php");
+    }
 
-if(($result_user) and ($result_user->rowCount() != 0)){
-    $rowUsuario = $result_user->fetch(PDO::FETCH_ASSOC);
-}
-else {
-    $_SESSION['msg'] = '<p>Error : usuario nao encontrado</p>';
-    header("Location:listar.php");
-}
+    //4 - Caso  o $id não esteja vazio, pesquisar pelo usuário no banco de dados.
+    $query_usuario = "SELECT id, matricula, nome, email, estatus, dtcadastro FROM usuarios WHERE id=$id LIMIT 1";
+
+    //5 - Preparando a query
+    $result_usuario = $conn->prepare($query_usuario);
+
+    //6 - Executando a consulta
+    $result_usuario->execute();
+
+    //7- Verificar se encontrou usuários no Banco
+    if(($result_usuario) and ($result_usuario->rowCount() !=0)){
+
+        //Armazenar os dados em um Array Associativo
+        $row_usuario = $result_usuario->fetch(PDO::FETCH_ASSOC);
+
+    }else{
+        $_SESSION['msg'] = "<p style='color:#f00;'>Erro: Usuário não encontrado</p>";
+        header("Location: listar.php");
+    }
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar</title>
-</head>
-<style>
-    body{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    .container{
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    <title>Atualização de registros</title>
 
-    }
-    .container form{
-        background-color: rgb(219, 219, 218);
-        padding: 1rem;
-        font-family:'Courier New', Courier, monospace;
-        display: flex;
-        justify-content: start;
-        flex-direction: column;
-        width: 600px;
-    }
-    header{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        background-color: black;
-        color: white;
-        margin-bottom: 2rem;
-    }
-    form label{
-        margin: 1rem 0;
-    }
-    form input{
-        width: 100%;
-        height: 2rem;
-    }
-</style>
+    <style>
+        .edicao{
+            width:100%;
+            max-width: 700px;
+            margin: 10px auto;
+            background-color: rgb(219, 218, 218);
+            padding: 20px;
+            border-radius: 5px;
+            margin-top: 10px;
+            font-family: Arial; 
+        }
+
+        input{
+            width: 70%;
+            padding: 10px 5px;
+            border-radius: 5px;
+            outline-color: #cdf;
+        }
+
+        label{
+            font-weight: bold;
+        }
+
+        .atualizar{
+            text-align: center;
+            outline-color: #cdf;
+        }
+
+        header{
+            text-align:center;
+            padding-bottom: 20px;
+            padding-top: 20px;
+        }
+    </style>
+</head>
 <body>
-    <?php
-    $dados = filter_input(INPUT_POST , FILTER_DEFAULT);
-    if (!empty($dados['EditUsuario'])){
+<?php
+    //8- Receber dados do formulário através do metodo POST e armazenar em um array
+    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+    //9- Verificar se o usuário clicou no botão "Atualizar"
+    if(!empty($dados['EditUsuario'])){
+
+        //Validação de input não nulo
         $empty_input = false;
 
-        array_map('trim' , $dados);
+        //Retirar espaços em branco no início e no final
+        array_map('trim', $dados);
 
-        if(in_array('', $dados)){
+        //10- Verificar se há compos em branco
+        if(in_array("", $dados)){
+
             $empty_input = true;
-            echo '<p>Precncha todos os campos</p>';
-        }elseif (!filter_var($dados['email'] , FILTER_VALIDATE_EMAIL)) {
-            $empty_input = true;
-            echo '<p>Precncha email valido</p>';
-        }if(!$empty_input){
-            $query_user = "UPDATE usuarios SET matricula=:matricula , nome=:nome , email=:email , estatus=:estatus WHERE id=:id";
-            $edit_user =  $con->prepare($query_user);
+            echo "<p style='color: #f00;'> Erro: Necessário preencher todos os campos!</p>";
+         
+        //11- Verificar se a estrutura do e-mail é válida   
+        }elseif(!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)){
+            $empty_input=true;
+            echo "<p style='color: #f00;'> Erro: Necessário preencher com e-mail válido!</p>";
 
-            $edit_user->bindParam(':matricula' , $dados['matricula'] , PDO::PARAM_INT);
-            $edit_user->bindParam(':nome' , $dados['nome'] , PDO::PARAM_STR);
-            $edit_user->bindParam(':email' , $dados['email'] , PDO::PARAM_STR);
-            $edit_user->bindParam(':estatus' , $dados['estatus'] , PDO::PARAM_STR);
-            $edit_user->bindParam(':id' , $id , PDO::PARAM_INT);
+        }
 
-            if($edit_user->execute()){
-                echo '<p>usuairo atualizado</p>';
-                header('Location:listar.php');
+        //12- Verificar se não há erros. Se verdadeiro atualizar o banco de dados
+        if(!$empty_input){
+
+            //Implementando o UPDATE  no Banco de Dados
+            $query_up_usuario = "UPDATE usuarios SET matricula=:matricula, nome=:nome, email=:email, estatus=:estatus WHERE id=:id";
+
+            //Preparando a query
+            $edit_usuario = $conn->prepare($query_up_usuario);
+
+            //13- Passando os valores do vetor de $dados para os pseudo-nomes
+            $edit_usuario->bindParam(':matricula', $dados['matricula'], PDO::PARAM_INT);
+            $edit_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+            $edit_usuario->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+            $edit_usuario->bindParam(':estatus', $dados['estatus'], PDO::PARAM_STR);
+            $edit_usuario->bindParam(':id', $id, PDO::PARAM_INT);
+
+            //14- Verificar se a execução da query foi realizada com sucesso!
+            if($edit_usuario->execute()){
+                $_SESSION['msg'] = "<p style='color: green;'>Usuário atualizado com sucesso!</p>";
+                header("Location: listar.php");
             }else{
-                echo '<p>usuairo nao atualizado</p>';
+                echo "<p style='color: #f00;'>Usuário não atualizado!</p>";
             }
         }
+
     }
-    ?>
+
+
+
+?>
     <header>
-        <h1 style="text-align: center;">Alteração de Registros</h1>
+        <h1>Edição de Registros</h1>
     </header>
-    <div class="container">
-        <form class="form" action="" method="POST">
-            <label for="">Matricula</label>
-            <input type="int" name="matricula" id="matricula" value="<?php 
-            if(isset($dados['matricula'])){
-                echo $dados['matricula'];
-            }elseif (isset($rowUsuario['matricula'])) {
-                echo $rowUsuario['matricula'];
-            }
-            ?>" placeholder="Digite uma Matricula"><br><br>
 
-            <label for="">Nome</label>
-            <input type="text" name="nome" id="nome" value="<?php 
-            if(isset($dados['nome'])){
-                echo $dados['nome'];
-            }elseif (isset($rowUsuario['nome'])) {
-                echo $rowUsuario['nome'];
-            }
-            ?>" placeholder="Digite um Nome"><br><br>
+    <!--Criando o formulário para edição de registros-->
+    <div class="edicao">
+        <form action="" method="POST">
+            <label>Matrícula: </label>
+            <input type="int" name="matricula" id="matricula" placeholder="Digite uma matrícula" value="<?php 
+                //Verificar se veio dados do usuario pelo formulário. Se verdadeiro, manter os dados
+                if(isset($dados['matricula'])){
+                    echo $dados['matricula'];
 
-            <label for="">Email</label>
-            <input type="email" name="email" id="email" value="<?php 
-            if(isset($dados['email'])){
-                echo $dados['email'];
-            }elseif (isset($rowUsuario['email'])) {
-                echo $rowUsuario['email'];
-            }
-            ?>" placeholder="Digite um Email"><br><br>
-            <label for="">Status</label>
-            <input type="text" name="status" id="stats" value="<?php 
-            if(isset($dados['estatus'])){
-                echo $dados['estatus'];
-            }elseif (isset($rowUsuario['estatus'])) {
-                echo $rowUsuario['estatus'];
-            }
-            ?>" placeholder="Digite um Estatus"><br><br>
-            <div style="margin-top: 1rem;">
+                //Mostrar os dados que veio do banco de dados
+                }elseif(isset($row_usuario['matricula'])){
+                    echo $row_usuario['matricula'];
+
+                }
+            ?>" width="90%"><br><br>
+
+            <label>Nome: </label>
+            <input type="text" name="nome" id="nome" placeholder="Nome completo" value="<?php 
+                //Verificar se veio dados do usuario pelo formulário. Se verdadeiro, manter os dados
+                if(isset($dados['nome'])){
+                    echo $dados['nome'];
+
+                //Mostrar os dados que veio do banco de dados
+                }elseif(isset($row_usuario['nome'])){
+                    echo $row_usuario['nome'];
+
+                }
+            ?>" width="90%"><br><br>
+
+            <label>E-mail: </label>
+            <input type="email" name="email" id="email" placeholder="Digite o melhor e-mail" value="<?php 
+                //Verificar se veio dados do usuario pelo formulário. Se verdadeiro, manter os dados
+                if(isset($dados['email'])){
+                    echo $dados['email'];
+
+                //Mostrar os dados que veio do banco de dados
+                }elseif(isset($row_usuario['email'])){
+                    echo $row_usuario['email'];
+
+                }
+            ?>" width="90%"><br><br>
+
+            <label>Status: </label>
+            <input type="text" name="estatus" id="estatus" value="<?php 
+                //Verificar se veio dados do usuario pelo formulário. Se verdadeiro, manter os dados
+                if(isset($dados['estatus'])){
+                    echo $dados['estatus'];
+
+                //Mostrar os dados que veio do banco de dados
+                }elseif(isset($row_usuario['estatus'])){
+                    echo $row_usuario['estatus'];
+
+                }
+            ?>" width="90%"><br><br><br>
+
+            <div class="atualizar">
                 <input type="submit" value="Atualizar" name="EditUsuario">
             </div>
         </form>
     </div>
+    <!--Fim do formulário-->
+    
 </body>
 </html>
--- listar.php
+++ listar.php
@@ -1,6 +1,7 @@
 <?php
 include('conexao.php');
 $con = dbinit();
+session_start();
 
 
 ?>
@@ -28,25 +29,40 @@ $con = dbinit();
 <body style="padding: 0;margin:0;">
     <header style="display:flex;align-items:center;justify-content:space-between;background-color:black;color:white;padding:0;margin:0;">
         <h1>Lista registros</h1>
+        <?php
+         if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+         }
+         unset($_SESSION['msg'])
+        ?> 
         <a style="color: white;text-decoration:none;" href="cadastro.php">Cadastro</a>
     </header>
     <?php
-    //http://localhost/projetofullstacksenac/listar.php?page=1 
+    /* -------------PAGINAÇÃO - PARTE_1 - INÍCIO ------------------*/
+       //1 - Criando uma variável para informar a página atual usando GET
+       // http://localhost/projetoacademico/listar2.php?page=1 
 
-    $actual_page_number = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
+       //2- Criar uma variável para receber o número da página atual da URL
+       $pagina_atual = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
 
-    $pagina = (!empty($actual_page_number)) ? $actual_page_number : 1;
-    
-    $limite_result = 5;
+       //3- Verificar se a numeração não foi enviada através da URL
+       $pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
 
-    $inicio = ($limite_result * $pagina) - $limite_result;
+       //4- Setar a quantidade de registros por página
+       $limite_result = 2;
+
+       //5- Calcular o início da visualização(precisamos identificar a partir de qual registro irá iniciar a próxima página)
+       $inicio = ($limite_result * $pagina) - $limite_result;
+
+    /* -------------PAGINAÇÃO - PARTE_1 - FIM ------------------*/
 
 
 
-    $query_users = $con->prepare("SELECT id , matricula ,nome , email , estatus , dtcadastro FROM usuarios ORDER BY dtcadastro ASC LIMIT $inicio , $limite_result ");
+        //2-Preparando a consulta de registro de usuários
+        $query_usuarios = $con->prepare("SELECT id, matricula, nome, email, estatus, dtcadastro FROM usuarios ORDER BY dtcadastro DESC LIMIT $inicio, $limite_result");
 
-
-    $query_users->execute();
+        //3-Executando a consulta
+        $query_usuarios->execute();
     ?>
     <div style="display: flex;justify-content:center;flex-direction:column">
     <table style="margin: 3rem 0;">
@@ -62,11 +78,11 @@ $con = dbinit();
             </tr>
         </thead>
         <?php
-        $row = $query_users->rowCount();
+        $row = $query_usuarios->rowCount();
 
         echo $row;
         if ($row != 0) {
-            while ($eachUser = $query_users->fetch(PDO::FETCH_ASSOC)) {
+            while ($eachUser = $query_usuarios->fetch(PDO::FETCH_ASSOC)) {
                 echo "<tr>";
                 extract($eachUser);
                     echo "<td>" . $id . "</td>";
@@ -75,7 +91,7 @@ $con = dbinit();
                     echo "<td>" . $email . "</td>";
                     echo "<td>" . $estatus . "</td>";
                     echo "<td>" . date("d/m/Y H:i:s", strtotime($dtcadastro)) . "</td>";
-                    echo "<td><a href='editar.php?id=$id' >[Editar]</a> <a href='' >[Excluir]</a> </td>"; 
+                    echo "<td><a href='editar.php?id=$id'>[Editar]</a> <a href=#>[Excluir]</a> </td>";
                 echo "</tr>";
             }
         ?>
@@ -85,29 +101,51 @@ $con = dbinit();
     <br>
     <br>
     <?php
+    /* -------------PAGINAÇÃO - PARTE_2 - INÍCIO ------------------*/
+    //6- Contar a quantidade de registros no banco de dados
+    $query_qnt_registros = $con->prepare("SELECT COUNT(id) AS num_result FROM usuarios");
+    $query_qnt_registros->execute();
 
-    $query_qnt = $con->prepare("SELECT COUNT(id) AS num_result FROM usuarios");
-    $query_qnt->execute();
+    $row_qnt_registros = $query_qnt_registros->fetch(PDO::FETCH_ASSOC);
 
-    $row_qnt = $query_qnt->fetch(PDO::FETCH_ASSOC);
-    
-    $qtn_pages = ceil($row_qnt['num_result'] / $limite_result);
+    //7-Identificar a quantidade de páginas para exibir todos os registros (CEIL)
+    $qnt_pagina = ceil($row_qnt_registros['num_result'] / $limite_result);
 
-    $max_link = 2;
+    //8- Criar uma variável para informar o máximo de links na página
+    $maximo_link = 2;
 
-    for($previos_page = $pagina - $max_link; $previos_page <= $pagina - 1; $previos_page++){
-        if ($previos_page >= 1 ){
-            echo "<a href='listar.php?page=$previos_page'>$previos_page</a>";
+    //9- Mostrar o link da primeira página
+    echo "<a href='listar.php?page=1'>Primeira</a> ";
+
+    //10- Listar os links anteriores da página atual (FOR)
+    for($pagina_anterior = $pagina - $maximo_link; $pagina_anterior <= $pagina-1; $pagina_anterior++){
+        
+        if($pagina_anterior >= 1){
+            echo "<a href='listar.php?page=$pagina_anterior'>$pagina_anterior</a> ";
         }
     }
-    for($pos_page = $pagina + 1; $pos_page <= $pagina + $max_link; $pos_page++){
-        if ($pos_page <= $qtn_pages){
-            echo "<a href='listar.php?page=$pos_page'>$pos_page</a>";
+
+    //11- Mostrar a página atual
+    echo "$pagina ";
+
+    //12- Listar os links posteriores a página atual (FOR)
+    for($pagina_posterior = $pagina + 1; $pagina_posterior <= $pagina + $maximo_link; $pagina_posterior++){
+        
+        if($pagina_posterior <= $qnt_pagina){
+            echo "<a href='listar.php?page=$pagina_posterior'>$pagina_posterior</a> ";
         }
     }
 
-    } else {
-        echo 'sem usuarios ';
+    //Link da última página
+    echo "<a href='listar.php?page=$qnt_pagina'>Ultima</a> ";
+
+
+
+    /* -------------PAGINAÇÃO - PARTE_2 - FIM ------------------*/   
+
+
+    }else{
+        echo "<p style='color:red;'>Erro: Usuário não encontrado</p>";
     }
 
     ?>
